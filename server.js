8913c90baa0fc1fbf590895c57e2f56b89136268
akshathatallur <30804136+akshathatallur@users.noreply.github.com>
2017-08-13 23:24:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,10 +77,12 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName = req.params.arictleName;
-res.send(createTemplate(articles[articleName]));
+app.get('/:articleName', function(req,res){
+    var articleName= req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
+
+
 
 app.get('/ui/style.css', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
