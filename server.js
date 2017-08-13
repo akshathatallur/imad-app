4325244796e6faf0eb9e75bbf58e8f196780f523
akshathatallur <30804136+akshathatallur@users.noreply.github.com>
2017-08-13 20:30:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,102 +6,94 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-   `article-one`: {
-  
- title: 'Article one | Akshatha Tallur',
- heading: 'Article one',
- date: '13 August 2017',
- content: `
-          <p> 
-            THis is the content for my first article.
-            </p> `
- 
- },
-   `article-two`: {
-    
- title: 'Article two | Akshatha Tallur',
- heading: 'Article Two',
- date: '13 August 2017',
- content: `
-          <p> 
-            THis is the content for my second article.
-            </p> `
- 
+'article-one': {
+title: 'Article one .. Im Varun',
+heading: 'Article One',
+date: 'August 5,2017',
+content: `
+<p>
+This is the conent for my first article.
+</p> `
+},
+'article-two': {
+title: 'Article Two .. Im Varun',
+heading: 'Article Two',
+date: 'August 10,2017',
+content:` 
+<p>
+This is the conent for my second article. 
+</p>`
 
-  },
-   `article-three`: {
-    
- title: 'Article Three | Akshatha Tallur',
- heading: 'Article Three',
- date: '13 August 2017',
- content: `
-          <p> 
-            THis is the content for my third article.
-            </p> `
- 
+},
+'article-three': {
+  title: 'Article Three .. Im Varun',  
+  heading: 'Article Three',
+  date: 'August 23,2017',
+  content: `
+            <p>
+                     This is the conent for my third article.      
+            </p>`
 
-  }
+}
 };
 
-
-function createTemplate(data){
-    var title = data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    
-    var htmlTemplate = `
-    <html>
-        <head>
-            <title>
-               ${title}
-            </title>
-            <meta name="viewpoint" content="width-device-width,intial-scale=1"/>
-            <link href="/ui/style.css" rel="stylesheet" />
-         </head>
-        
-       
-        <body>
-            <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr/>
-            <h3> ${heading} </h3>
-            <div> ${date}</div>
-            <div>
-                <p> 
-                ${content}
-                </p>
-            </div>
-            </div>
-        </body>
-    </html>
-    `;
-    return htmlTemplate;
-}  
+function createTemplate (data) {
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate = 
+<html>
+<head>
+<title>
+${title} 
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+<div class="container">
+<div>
+<a href="/">Home</a>
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>
+;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function(req,res){
-    var articleName= req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+
+app.get('/:articleName', function (req, res){
+    var articleName = req.params.arictleName;
+res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(IMAD course app listening on port ${port}!);
 });
